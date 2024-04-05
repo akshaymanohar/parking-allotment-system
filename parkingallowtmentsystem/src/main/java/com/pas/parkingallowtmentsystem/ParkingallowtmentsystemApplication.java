ba809a40e70e097da51588f77c4860c528665043
akshaymanohar <akshaymanohar37@gmail.com>
2024-04-05 07:38:46 -0700

    parking allowtment spring project

---
+++ parkingallowtmentsystem/src/main/java/com/pas/parkingallowtmentsystem/ParkingallowtmentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.pas.parkingallowtmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingallowtmentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingallowtmentsystemApplication.class, args);
+	}
+
+}
